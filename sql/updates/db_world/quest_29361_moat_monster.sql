8ffae8b70dbd8bacfe1a7dd83e45ddd34a89fdc3
Pagani Walter <paganiwalter@gmail.com>
2020-01-19 04:15:48 -0300

    Eliminando el `smart_script`, dado que el spell da el crédito

--- sql/updates/db_world/quest_29361_moat_monster.sql
+++ sql/updates/db_world/quest_29361_moat_monster.sql
@@ -20,7 +20,4 @@ INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry
 
 DELETE FROM `smart_scripts` WHERE `entryorguid`=53590 AND `source_type`=0;
 
-UPDATE `creature_template` SET `AIName`='SmartAI', `ScriptName`=''  WHERE `entry`=53590;
-
-INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
-(53590, 0, 0, 0, 8, 0, 100, 0, 99508, 1, 0, 0, 33, 53592, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "On Spellhit - Quest Credit");
+UPDATE `creature_template` SET `AIName`='', `ScriptName`=''  WHERE `entry`=53590;
