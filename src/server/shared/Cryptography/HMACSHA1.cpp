f0783118286d67ff7da1709de528ed46da752fac
Pagani Walter <paganiwalter@gmail.com>
2020-01-28 14:15:32 -0300

    Eliminando & y transformando la variable en un puntero.

--- src/server/shared/Cryptography/HMACSHA1.cpp
+++ src/server/shared/Cryptography/HMACSHA1.cpp
@@ -22,30 +22,30 @@
 
 HmacHash::HmacHash(uint32 len, uint8 *seed)
 {
-    HMAC_CTX_init(&m_ctx);
-    HMAC_Init_ex(&m_ctx, seed, len, EVP_sha1(), NULL);
+    HMAC_CTX_new();
+    HMAC_Init_ex(m_ctx, seed, len, EVP_sha1(), NULL);
 }
 
 HmacHash::~HmacHash()
 {
-    HMAC_CTX_cleanup(&m_ctx);
+    HMAC_CTX_get_md(m_ctx);
 }
 
 void HmacHash::UpdateData(const std::string &str)
 {
-    HMAC_Update(&m_ctx, (uint8 const*)str.c_str(), str.length());
+    HMAC_Update(m_ctx, (uint8 const*)str.c_str(), str.length());
 }
 
 void HmacHash::Finalize()
 {
     uint32 length = 0;
-    HMAC_Final(&m_ctx, (uint8*)m_digest, &length);
+    HMAC_Final(m_ctx, (uint8*)m_digest, &length);
     ASSERT(length == SHA_DIGEST_LENGTH)
 }
 
 uint8 *HmacHash::ComputeHash(BigNumber* bn)
 {
-    HMAC_Update(&m_ctx, bn->AsByteArray(), bn->GetNumBytes());
+    HMAC_Update(m_ctx, bn->AsByteArray(), bn->GetNumBytes());
     Finalize();
     return (uint8*)m_digest;
 }
